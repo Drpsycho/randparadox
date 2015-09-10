d60b639b790d59ba930f71b53797a3701fb60789
Dmitry.Kharisov <drpsychoz@gmail.com>
2015-09-10 10:37:29 +0600

    return math to back

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 )
 
@@ -17,9 +16,6 @@ type t_stage struct {
 
 //колличество дверей
 const doorNum int = 3
-
-// для ожидания конца работы горутин
-var wg sync.WaitGroup
 
 func GetStage() t_stage {
 	var stage t_stage
@@ -46,7 +42,7 @@ func GetStage() t_stage {
 	return stage
 }
 
-func PickDoor(stage t_stage, isRevers bool) bool {
+func PickDoor(stage t_stage) (bool, bool) {
 	s1 := rand.NewSource(time.Now().UnixNano())
 	r := rand.New(s1)
 
@@ -57,7 +53,7 @@ func PickDoor(stage t_stage, isRevers bool) bool {
 		// рандомно генерим номер двери и пытаемся удалить
 		// если это не правильная дверь и не выбранная игроком, тогда удаляем
 		// т.е. выходим из цикла с номером удалённой двери
-		numDoorForRemove := r.Intn(doorNum)
+		numDoorForRemove = r.Intn(doorNum)
 
 		if numDoorForRemove != stage.rightDoor {
 			if numDoorForRemove != stage.choosenDoor {
@@ -68,42 +64,44 @@ func PickDoor(stage t_stage, isRevers bool) bool {
 	// fmt.Println("Delete door number ", numDoorForRemove)
 
 	// если isRevers правда меняем выбор
-	res := 0
+	res_with_repick := 0
+	res_without_repick := 0
 
 	for j := 0; j < 3; j++ {
 		if j != numDoorForRemove {
-			if isRevers {
-				if j != stage.choosenDoor {
-					// fmt.Println("Return door number - ", j)
-					res = j
-				}
-			} else {
-				if j == stage.choosenDoor {
-					// fmt.Println("Return door number - ", j)
-					res = j
-				}
+			if j != stage.choosenDoor {
+				// fmt.Println("Return door number - ", j)
+				res_with_repick = j
+			}
+
+			if j == stage.choosenDoor {
+				// fmt.Println("Return door number - ", j)
+				res_without_repick = j
 			}
 		}
 	}
 
 	// fmt.Println("Result is ", stage.doors[res])
-	return stage.doors[res]
+	return stage.doors[res_with_repick], stage.doors[res_without_repick]
 }
 
-func worker(steps int, repick bool) {
+func worker(steps int) {
 	result := 0
-
+	result2 := 0
 	for j := 0; j < steps; j++ {
 		//получаем с чем работать
 		stage := GetStage()
 		//выбираем дверь, если ок, плюсуем
-		if PickDoor(stage, repick) {
+		res_with_repick, res_without_repick := PickDoor(stage)
+		if res_with_repick {
 			result++
 		}
+		if res_without_repick {
+			result2++
+		}
 	}
-	fmt.Println("Repick is ", repick, " bingo - ", result, " steps = ", steps)
-
-	wg.Done()
+	fmt.Println("With Repick is bingo -    ", result, " steps = ", steps)
+	fmt.Println("Without Repick is bingo - ", result2, " steps = ", steps)
 }
 
 func main() {
@@ -119,10 +117,5 @@ func main() {
 	}
 
 	steps, _ := strconv.Atoi(os.Args[1])
-	wg.Add(2)
-	//запускаем две горутины с перевыбором двери(true) и без(false)
-	go worker(steps, true)
-	go worker(steps, false)
-
-	wg.Wait()
+	worker(steps)
 }
